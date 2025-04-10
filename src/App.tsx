0f378ce32f942a2cea32c876d1fcf25d4994d639
Cauã Campos <campos.eet@gmail.com>
2025-04-10 00:55:31 -0300

    :tada: Initialize project with all presets

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Folder } from "./components/Folder";
+
+export function App() {
+    return (
+        <div className="flex flex-col justify-center items-center w-full h-screen bg-green-800">
+            <Folder />
+        </div>
+    );
+}
